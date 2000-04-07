aba114cca468d35460e3f0d44c0f77fcde8d302b
agilmore <>
2000-04-07 19:49:51 +0000

    Changes to do commit/rollback on errors in insert values, also changes to get and use environment variables like the HDB applications written in C (get psswd_user, set role on app_user connection) Also usage statement and command line argument counting. Still needs precip, and site_ids for the two more sites.

--- gun_temps/src/guntemps2hdb.pl
+++ gun_temps/src/guntemps2hdb.pl
@@ -6,11 +6,18 @@ use DBI;
 
 my ($dbh, @cur_sdi, @value_date, @data_date);
 
+my($psswd_dbh);
+
 sub mydie {
-  $dbh->disconnect();
+  $dbh->disconnect;
   die @_;
 }
 
+sub passdie {
+    $psswd_dbh->disconnect;
+    die @_;
+}
+    
 #sites and datatypes desired. Not all site_ids and site_datatype_ids
 # have been created, yet.
 
@@ -25,10 +32,17 @@ sub mydie {
 %site_datatype_hash = ("max temp", undef, "ave temp", undef, "min temp", undef,
 		       "precip total", undef, "total wy precip", undef);
 
-#create connection to database, exceptions on error, no autocommit
+#check to see command line usage.
+if (scalar(@ARGV) <3)
+  {
+    print "$0 -- program to put gunnison climate data into HDB.\n";
+    print "Usage: $0 <filename> <hdbusername> <hdbpassword>\n";
+    exit (1);
 
-$dbh = DBI->connect("dbi:Oracle:uchdb", $ARGV[0], $ARGV[1],
-		    { RaiseError => 1, AutoCommit => 0 })|| mydie $DBI::errstr;
+  }
+
+#create connection to database
+connect_to_db($ARGV[1], $ARGV[2]);
 
 #$statement = "select * from hdb_data_source ";
 
@@ -40,36 +54,34 @@ $update_data_statement = "update r_day SET (value) = ( ?) where (site_datatype_i
 
 $check_data_statement = "select value from r_day where date_day = ? and site_datatype_id = ? ";
 
-
+open (INFILE, "$ARGV[0]") || mydie "Error: couldn't open input file $ARGV[0]";
 # skip past opening junk
 for ($i = 0; $i < 6; $i++)
-  { $line = <STDIN>; }
+  { $line = <INFILE>; }
 
 # get date of data
-$line = <STDIN>;
+$line = <INFILE>;
 $datestr = substr $line, 33, 8;
 
 (@data_date = Decode_Date_US($datestr)) ||
-  die "There was an error parsing the end date.\n";
+  die "There was an error parsing the date in the datafile!.\n";
 
 print "@data_date\n";
 
 #skip past header
 for ($i = 0; $i < 6; $i++)
-  { $line = <STDIN>; }
+  { $line = <INFILE>; }
 
-
-READ: while ($line = <STDIN>)
+READ: while ($line = <INFILE>)
 {
   chomp($line);
+  #end data if line is really short, or contains 78 dashes ('-')
   last READ if (length($line) < 5 || $line =~ m/-{78}/) ;
 
   #check to see if the site changed
   $cur_site_name = substr $line, 0, 20;
   if ($cur_site_name =~ /\S/g)
     { # if so, go get new site datatype ids
-#     print "$cur_site_name\n";
-#     print "$siteid_hash{$cur_site_name}\n";
       $site_id = $siteid_hash{$cur_site_name};
       get_SDI($site_id)      if $site_id;
     }
@@ -108,17 +120,15 @@ READ: while ($line = <STDIN>)
   if ($ave_temp == -99.9 ) {
     $ave_temp = undef;
   }
-#  print "@value_date\t";
-#  print "$site_id\t$cum_precip\t$max_temp\t$min_temp\t$ave_temp\n";
 
   if ($site_id) {
     insert_values(@value_date, $cum_precip, $max_temp, $min_temp, $ave_temp);
   }
 }
 
-#print check_value('29-mar-2000', 2369);
-
 $dbh->disconnect();
+
+#End main program, subroutines below
 
 sub check_value
   {
@@ -154,47 +164,55 @@ sub insert_values
     $datestr = sprintf("%02d-%.3s-%4d",$value_date[2],
 			   Month_to_Text($value_date[1]), $value_date[0]); 
 
-    $insth = $dbh->prepare($insert_data_statement) || mydie $insth->errstr;
-    $upsth = $dbh->prepare($update_data_statement) || mydie $upsth->errstr;
-
-    $i = 3;
-    foreach $datatype ("max temp", "min temp", "ave temp") 
-      {
-	$i++; undef $cur_val;
-	$cur_val = check_value($datestr, $site_datatype_hash{$datatype});
-	if (!defined($cur_val)) {
-	  print "inserting for $site_datatype_hash{$datatype}, date $datestr, value $_[$i], cur_val = undefined\n";
-	
-	  $insth->bind_param(1,$site_datatype_hash{$datatype});
-	  $insth->bind_param(2,$datestr);
-	  $insth->bind_param(3,$_[$i]);
-	  $insth->execute|| mydie $insth->errstr;
-	}
-	elsif (!defined $_[$i]) {
-	  #delete data if data is -99.9, otherwise do nothing.
-	  if ($cur_val == -99.9) {
-	    print "deleting bad data for $site_datatype_hash{$datatype}, date $datestr, value undefined, cur_val = $cur_val\n";
-	    $dbh->do("delete from r_day where site_datatype_id = $site_datatype_hash{$datatype} and date_day = '$datestr'");
+    eval {
+      $insth = $dbh->prepare($insert_data_statement) || mydie $insth->errstr;
+      $upsth = $dbh->prepare($update_data_statement) || mydie $upsth->errstr;
+      
+      $i = 3;
+      foreach $datatype ("max temp", "min temp", "ave temp") 
+	{
+	  $i++; undef $cur_val;
+	  $cur_val = check_value($datestr, $site_datatype_hash{$datatype});
+	  if (!defined($cur_val)) {
+	    print "inserting for $site_datatype_hash{$datatype}, date $datestr, value $_[$i], cur_val = undefined\n";
+	    
+	    $insth->bind_param(1,$site_datatype_hash{$datatype});
+	    $insth->bind_param(2,$datestr);
+	    $insth->bind_param(3,$_[$i]);
+	    $insth->execute|| mydie $insth->errstr;
+	  }
+	  elsif (!defined $_[$i]) {
+	    #delete data if data is -99.9, otherwise do nothing.
+	    if ($cur_val == -99.9) {
+	      print "deleting bad data for $site_datatype_hash{$datatype}, date $datestr, value undefined, cur_val = $cur_val\n";
+	      $dbh->do("delete from r_day where site_datatype_id = $site_datatype_hash{$datatype} and date_day = '$datestr'")|| mydie DBI::errstr;
+	    }
+	  }
+	  elsif ($cur_val == $_[$i]) {
+	    next;
+	  }
+	  elsif ($cur_val != $_[$i]) {
+	    print "updating for $site_datatype_hash{$datatype}, date $datestr, value $_[$i], cur_val = $cur_val\n";
+	    #update instead of insert
+	    $upsth->bind_param(1,$_[$i]);
+	    $upsth->bind_param(2,$site_datatype_hash{$datatype});
+	    $upsth->bind_param(3,$datestr);
+	    $upsth->execute|| mydie $upsth->errstr;
 	  }
 	}
-	elsif ($cur_val == $_[$i]) {
-	  next;
-	}
-	elsif ($cur_val != $_[$i]) {
-	  print "updating for $site_datatype_hash{$datatype}, date $datestr, value $_[$i], cur_val = $cur_val\n";
-	  #update instead of insert
-	  $upsth->bind_param(1,$_[$i]);
-	  $upsth->bind_param(2,$site_datatype_hash{$datatype});
-	  $upsth->bind_param(3,$datestr);
-	  $upsth->execute|| mydie $upsth->errstr;
-	}
-      }
-    
-    $upsth->finish;
-    $insth->finish();
-    
-
-  }
+      
+      $upsth->finish;
+      $insth->finish;
+    };
+    if ($@) {
+      $dbh->rollback;
+      print "$DBI::errstr\n";
+      mydie "Errors occurred during insert/update/deletion of data.";
+    }
+    else {
+      $dbh->commit;
+    }
+}
 
 
 sub get_SDI
@@ -211,6 +229,50 @@ sub get_SDI
 	$sth->fetch;
       }
     
-    $sth->finish();
+    $sth->finish;
   }
 
+# HDB does something interesting with the app_user 
+# app_user has the permission to insert into the database, but only
+# if the app_role is enabled, which is not by default
+# the applications get around this by connecting to the database as 
+# psswd_user (from the environment) and getting the password to 
+# turn on this role, and then connecting as app_user and 
+# enabling this role.
+# This code does that, while also taking the name of the database
+# from the environment, and the app_user name and password from the
+# command line
+sub connect_to_db
+  {
+    my $user=$_[0];
+    my $pass=$_[1];
+    my $approlepass;
+
+    if (!defined($psswduser = $ENV{PSSWD_USER})) {
+      die "Environment variable PSSWD_USER not set...\n";
+    }
+    if (!defined($dbname = $ENV{HDB_LOCAL})) {
+      die "Environment variable HDB_LOCAL not set...\n";
+    }
+    if (!defined($approle = $ENV{APP_ROLE})) {
+      die "Environment variable APP_ROLE not set...\n";
+    }
+
+# create connections, exceptions on error, no autocommit
+
+    $psswd_dbh = DBI->connect("dbi:Oracle:$dbname", $psswduser, $dbname,
+			{ RaiseError => 1, AutoCommit => 0 })|| mydie $DBI::errstr;
+    $psswd_statement = "select psswd from role_psswd where UPPER(ROLE) = UPPER('$approle')";
+
+    $psswdsth = $psswd_dbh->prepare($psswd_statement) || passdie $psswdsth->errstr;
+    $psswdsth->execute || passdie $psswdsth->errstr;
+    $psswdsth->bind_col(1,\$approlepass);
+    $psswdsth->fetch;
+    $psswdsth->finish;
+    $psswd_dbh->disconnect;
+
+    $dbh = DBI->connect("dbi:Oracle:$dbname", $user, $pass,
+			{ RaiseError => 1, AutoCommit => 0 })|| mydie $DBI::errstr;
+
+    $dbh->do("set role $approle identified by $approlepass")|| mydie $DBI::errstr;
+  }
