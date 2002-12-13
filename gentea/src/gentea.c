02288044c9b38b9a9936085790fc96c63933c7fd
agilmore <>
2002-12-13 17:15:36 +0000

    Changes to clean up source

--- gentea/src/gentea.c
+++ gentea/src/gentea.c
@@ -5,6 +5,8 @@
 #include <time.h>
 #include "gd.h"
 #include "gdfonts.h"
+#include "dbutils.h"
+#include "utils.h"
 
 struct datetime {
    int yr;
@@ -15,138 +17,148 @@ struct datetime {
    int sc;
 };
 
+/* colors used in teacup.c */
 extern int foreground,background,foregroundtext;
 
-extern char buf[100];
+/* defined in readdata.pc (embedded sql in C) */
+float readone(int,int,char*);
 
-void main(int argc,char **argv) 
+/* defined in teacup.c */
+void teacup(gdImagePtr,gdPoint,int,double,double,char *);
+void lineprint(gdImagePtr,gdPoint,float,char *,char *);
+void lineprint2(gdImagePtr,gdPoint,float,char *,char *);
+
+int main(int argc,char **argv) 
 {
-   	float readone(char *,char *,struct datetime *);
-	void teacup(gdImagePtr,gdPoint,int,double,double,char *);
-	void lineprint(gdImagePtr,gdPoint,float,char *,char *);
-	void lineprint2(gdImagePtr,gdPoint,float,char *,char *);
-	gdImagePtr im;	
-	FILE *ofs,*ifs,*cfs;	
-	int retval,row,col,size,red,white,blue,green,sofar,i,j;
-	time_t t;
-	char timestring[50],drawitem[30],description[100],line[100],units[30];
-        float value,capacity;
-        int station, pcode;
-	gdPoint points[4],base;
-    struct tm *timep;
-    struct datetime dt;
+   gdImagePtr im;	
+   FILE *ofs,*ifs,*cfs;	
+   int retval,row,col,size,red,white,blue,green,sofar,i,j;
+   time_t t;
+   char timestring[50],drawitem[30],description[100],line[100],units[30];
+   char maxdate[25];
+   float value,capacity;
+   int site, datatype;
+   gdPoint base;
+   struct tm *timep;
+   struct datetime dt;
 
+   if(argc!=4)
+   {
+      printf("Usage: gentea infile outfile configfile\n");
+      exit(1);
+   }
+   if((ifs=fopen(argv[1],"rb"))==NULL)
+   {
+      printf("Error opening input file: %s\n",argv[1]);
+      exit(1);
+   }
+   if((ofs=fopen(argv[2],"wb"))==NULL)
+   {
+      printf("Error opening output file: %s\n",argv[2]);
+      exit(1);
+   }
+   if((cfs=fopen(argv[3],"r"))==NULL)
+   {
+      printf("Error opening configuration file: %s\n",argv[3]);
+      exit(1);
+   }
 
-	if(argc!=4)
-	{
-		printf("Usage: gentea infile outfile configfile\n");
-		exit(0);
-	}
-	if((ifs=fopen(argv[1],"rb"))==NULL)
-	{
-		printf("Error opening input file: %s\n",argv[1]);
-		exit(0);
-	}
-	if((ofs=fopen(argv[2],"wb"))==NULL)
-	{
-		printf("Error opening output file: %s\n",argv[2]);
-		exit(0);
-	}
-	if((cfs=fopen(argv[3],"r"))==NULL)
-	{
-		printf("Error opening configuration file: %s\n",argv[3]);
-		exit(0);
-	}
+   /*read in the image*/
+   im = gdImageCreateFromPng(ifs);
+   white = gdImageColorAllocate(im, 255, 255, 255);	
+   blue = gdImageColorAllocate(im, 100,100, 255);		
+   red = gdImageColorAllocate(im,255,0,0);
+   green = gdImageColorAllocate(im,0,220,0);
+   foreground=blue;
+   background=white;
+   foregroundtext=red;
 
-	/*read in the image*/
-	im = gdImageCreateFromPng(ifs);
-	white = gdImageColorAllocate(im, 255, 255, 255);	
-	blue = gdImageColorAllocate(im, 100,100, 255);		
-	red = gdImageColorAllocate(im,255,0,0);
-	foreground=blue;
-	background=white;
-	foregroundtext=red;
+   /* connect to the database */
+   char *dbName;
+   int result;
+  
+   dbName = NULL; 
+   dbName = getenv("HDB_LOCAL");
 
+   if (dbName == NULL)
+   {
+      PrintError("Environment variable HDB_LOCAL not set...\n");
+      exit(ERROR);
+   }
+  
+  
+   if  ((result = SqlConnect (dbName, "app_user", dbName)) == ERROR)
+      exit (ERROR);
 
-	/*get the current date & time and print on the image*/
-	t=time((time_t)0);
-	t-=86400;             /*subtract a day*/
-	timep = localtime(&t);
-	dt.yr = timep->tm_year;
-    dt.yr+=1900;              /*local time subtracts 1900*/
-    dt.mn = timep->tm_mon+1;
-    dt.dy = timep->tm_mday;
-    dt.hr=0;dt.mi=0;dt.sc=0;
+   /*get the current date & time and print on the image*/
+   t=time((time_t)0);
+   t-=86400;             /*subtract a day*/
+   timep = localtime(&t);
+   dt.yr = timep->tm_year;
+   dt.yr+=1900;              /*local time subtracts 1900*/
+   dt.mn = timep->tm_mon+1;
+   dt.dy = timep->tm_mday;
+   dt.hr=0;dt.mi=0;dt.sc=0;
 
-	sprintf(timestring,"%02d/%02d/%4d",dt.mn,dt.dy,dt.yr);
-    gdImageFilledRectangle(im,0,0,100,14,background);    
-    gdImageString(im,gdFontSmall,0,0,(unsigned char *)timestring,foreground);
+   sprintf(timestring,"%02d/%02d/%4d",dt.mn,dt.dy,dt.yr);
+   gdImageFilledRectangle(im,0,0,100,14,background);    
+   gdImageString(im,gdFontSmall,0,0,(unsigned char *)timestring,foreground);
 	
-	
-	while(fgets(line,100,cfs)!=NULL)
-	{
-	
-		retval=sscanf(line,"%s %d %d %d %d %d %f %n",drawitem,&row,&col,&station,
-		                                     &pcode,&size,&capacity,&sofar);
+
+   /* read the config file, get the data, put it on the image per line */
+   while(fgets(line,100,cfs)!=NULL)
+   {
+
+      retval=sscanf(line,"%30s %d %d %d %d %d %f %n",drawitem,&row,&col,&site,
+                    &datatype,&size,&capacity,&sofar);
+
+      for(i=sofar,j=0;i<100 && line[i]!='\n';i++,j++)
+         description[j]=line[i];
+      description[j]='\0';
+
+      if(retval==7 && (drawitem[0]=='t' || drawitem[0]=='T'))
+      {
+         base.x=row;
+         base.y=col;
+         value = readone(site,datatype,maxdate);
+         teacup(im,base,size,capacity,value,description);
+      }
+
+      else
+      {
+         retval=sscanf(line,"%30s %d %d %d %d %30s %n",
+                       drawitem,&row,&col,&site,
+                       &datatype,units,&sofar);
 		
-		for(i=sofar,j=0;i<100 && line[i]!='\n';i++,j++)
-			description[j]=line[i];
-		description[j]='\0';
-
-	
-		if(retval==7 && (drawitem[0]=='t' || drawitem[0]=='T'))
-		{
-			base.x=row;
-			base.y=col;
-			value = readone(station,pcode,&dt);
-			teacup(im,base,size,capacity,value,description);
-		}
-
-		else if(retval==5 && (drawitem[0]=='c' || drawitem[0]=='C'))
-		{
-
-			base.x=row;
-			base.y=col;
-			value = readone(station,pcode,&dt);
-                        char str_site_id[20];
-                        sprintf(str_site_id,"%d",station);
-		    lineprint(im,base,value,str_site_id,"cfs");
-		}
-		else
-		{
-                        printf("Not Here\n");
-
-                   retval=sscanf(line,"%s %d %d %d %d %s %n",
-                                 drawitem,&row,&col,&station,
-                                 &pcode,units,&sofar);
-		
-			for(i=sofar,j=0;i<100 && line[i]!='\n';i++,j++)
-				description[j]=line[i];
-			description[j]='\0';
+         for(i=sofar,j=0;i<100 && line[i]!='\n';i++,j++)
+            description[j]=line[i];
+         description[j]='\0';
 
                         
-			if(retval==6 && (drawitem[0]=='l' || drawitem[0]=='L'))
-			{
-				base.x=row;
-				base.y=col;
-				value = readone(station,pcode,&dt);
-                                printf("%f\n",value);
-				lineprint2(im,base,value,description,units);
-			}
-			else
-				printf("Error interpreting draw request: %d\n",retval);
-		}
-	}	
+         if(retval==6 && (drawitem[0]=='l' || drawitem[0]=='L'))
+         {
+            base.x=row;
+            base.y=col;
+            value = readone(site,datatype,maxdate);
+            lineprint2(im,base,value,description,units);
+         }
+         else
+            printf("Error interpreting draw request: %d\n",retval);
+      }
+   }
+
+   if ((result = SqlDisconnect ()) == ERROR)
+      exit(ERROR);
 
 
+   /* Output the image to the disk file. */	
 
-	/* Output the image to the disk file. */	
-
-	gdImagePng(im, ofs);	
-	fclose(ifs);
-	fclose(ofs);
-	fclose(cfs);
+   gdImagePng(im, ofs);	
+   fclose(ifs);
+   fclose(ofs);
+   fclose(cfs);
         
-        gdImageDestroy(im);
+   gdImageDestroy(im);
 
+   exit(0);
 }
