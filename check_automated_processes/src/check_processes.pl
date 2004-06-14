5bf0829355fbd96a3f65aace3d2d411a3011ce50
agilmore <>
2004-06-14 18:02:45 +0000

    Many changes to allow procedure to give more information about failures, also to check on the status of the realtime loading application.

--- check_automated_processes/src/check_processes.pl
+++ check_automated_processes/src/check_processes.pl
@@ -1,9 +1,11 @@
 #!/usr/bin/perl -w
+
 use strict;
 
+# check on all the crontab applications
 open INFILE, 'automatedapps';
 
-my ($app,$errfile,$line,@dirent,$HDB_ENV);
+my ($app,$errfile,$expectfile,$cmdname,$ps,$line,@dirent,$HDB_ENV);
 while (<INFILE>)
 {
   chomp;
@@ -14,16 +16,39 @@ while (<INFILE>)
 
   $errfile =~ s /\$HDB_ENV/$HDB_ENV/e;
 
-#  print "$app, $errfile\n";
-
   if (! (@dirent = stat($errfile))) {
     print "File not found, '$errfile'\n";
     next;
   }
-#  print "@dirent\n";
-
+  #  print "@dirent\n";
 
   if ($dirent[7] != 0) {
-   print "Application $app had an error! $errfile is not empty\n";
- }
+    $expectfile = $errfile . ".expected";
+    if (stat($expectfile)) {
+      if (`diff $errfile $expectfile`) {
+        print "Application $app had an error! $errfile is not as expected\n";
+        open ERRFILE, $errfile;
+        print <ERRFILE>;
+      }
+    } else {
+      print "Application $app had an error! $errfile is not empty\n";
+      open ERRFILE, $errfile;
+      print <ERRFILE>;
+    }
+  }
 }
+
+close INFILE;
+
+# check on the realtime applications
+open INFILE, 'realtimeapps';
+while (<INFILE>)
+{
+  chomp;
+  ($app,$cmdname) = split '=';
+  $ps = `ps -fuuchdba|grep $cmdname|grep -v grep`;
+  if (!$ps) {
+    print"Realtime application $app down!!!\n";
+  }
+}
+
