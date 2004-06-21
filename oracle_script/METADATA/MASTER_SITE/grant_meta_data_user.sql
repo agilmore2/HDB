8c69129ffc5eb78676d038aac9e79c8de3c276f7
cmarra <>
2004-06-21 21:07:31 +0000

    Modified to accomodate grant of hdb_meta_role to meta_data_user

--- oracle_script/METADATA/MASTER_SITE/grant_meta_data_user.sql
+++ oracle_script/METADATA/MASTER_SITE/grant_meta_data_user.sql
@@ -1,13 +1,16 @@
 CREATE OR REPLACE PROCEDURE grant_meta_data_user (table_name IN VARCHAR,
   do_grant IN BOOLEAN) IS
 BEGIN
+/* DO NOTHING; no longer needed since meta_data_user has hdb_meta_role
+   Do not delete procedure because is called by Meta Data App
+
    IF do_grant THEN
      EXECUTE IMMEDIATE 'grant insert, update, delete on ' || table_name || ' to meta_data_user';
    ELSE
      EXECUTE IMMEDIATE 'revoke insert, update, delete on ' || table_name || '
 from meta_data_user';
    END IF;
-  
+*/  
    commit;
 END; 
 /
