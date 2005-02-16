228cd9990d955668088e42aab352d0a57b79b6cc
agilmore <>
2005-02-16 18:10:44 +0000

    Fixed unquoted apostrophe in last line of file.

--- oracle_script/PERMISSIONS/local_user.sql
+++ oracle_script/PERMISSIONS/local_user.sql
@@ -1,24 +1,52 @@
-/* Tom */
-/* create user tom identified by tom */
-/* default tablespace HDB_user quota unlimited on HDB_user */
-/* temporary tablespace HDB_temp quota unlimited on HDB_temp; */
+/* new user agilmore*/
+create user agilmore identified by newagilmore
+default tablespace HDB_user quota unlimited on HDB_user
+temporary tablespace HDB_temp quota unlimited on HDB_temp;
+grant connect to agilmore;
+grant hdb_meta_role to agilmore;
+grant ref_meta_role to agilmore;
+grant savoir_faire to agilmore;
+grant model_role to agilmore;
+alter user agilmore default role connect, savoir_faire;
 
-/* grant connect to tom; */
-/* grant savoir_faire to tom; */
-/* grant app_role to tom; */
-/* grant hdb_meta_role to tom; */
-/* grant ref_meta_role to tom; */
-/* grant model_role to tom; */
-/* alter user tom */
-/* default role connect, savoir_faire; */
+/* new user dking*/
+create user dking identified by dking_new
+default tablespace HDB_user quota unlimited on HDB_user
+temporary tablespace HDB_temp quota unlimited on HDB_temp;
+grant connect to dking;
+alter user dking default role connect;
 
-/* read only */
-/* create user coroneos identified by coroneos */
-/* default tablespace HDB_user quota unlimited on HDB_user */
-/* temporary tablespace HDB_temp quota unlimited on HDB_temp; */
-/* grant connect to coroneos; */
+/* new user pdavidson*/
+create user pdavidson identified by pdavidson_new
+default tablespace HDB_user quota unlimited on HDB_user
+temporary tablespace HDB_temp quota unlimited on HDB_temp;
+grant connect to pdavidson;
+grant hdb_meta_role to pdavidson;
+grant ref_meta_role to pdavidson;
+grant savoir_faire to pdavidson;
+grant model_role to pdavidson;
+alter user pdavidson default role connect, savoir_faire;
 
+/* new user rclayton*/
+create user rclayton identified by rclayton_new
+default tablespace HDB_user quota unlimited on HDB_user
+temporary tablespace HDB_temp quota unlimited on HDB_temp;
+grant connect to rclayton;
+grant hdb_meta_role to rclayton;
+grant ref_meta_role to rclayton;
+grant savoir_faire to rclayton;
+grant model_role to rclayton;
+alter user rclayton default role connect, savoir_faire;
 
-
+/* new user cmarra*/
+create user cmarra identified by cmarra_new
+default tablespace HDB_user quota unlimited on HDB_user
+temporary tablespace HDB_temp quota unlimited on HDB_temp;
+grant connect to cmarra;
+grant hdb_meta_role to cmarra;
+grant ref_meta_role to cmarra;
+grant savoir_faire to cmarra;
+grant model_role to cmarra;
+alter user cmarra default role connect, savoir_faire;
 
 
--- oracle_script/PERMISSIONS/permissions.sh
+++ oracle_script/PERMISSIONS/permissions.sh
@@ -3,10 +3,10 @@
 # The following calls create the users, table, and triggers/procedures
 # needed to implement new HDB permissions scheme.
 
-sqlplus sys/hdb2 @./PERMISSIONS/create_psswd_user.sql $HDB_LOCAL > create_psswd_user.out;
-sqlplus sys/hdb2 < ./PERMISSIONS/set_role_passwords.sql > set_role_passwords.out;
+sqlplus 'uchdba/uchdbdev as sysdba' @./PERMISSIONS/create_psswd_user.sql $HDB_LOCAL > create_psswd_user.out;
+sqlplus 'uchdba/uchdbdev as sysdba' ./PERMISSIONS/set_role_passwords.sql > set_role_passwords.out;
 sqlplus psswd_user/$HDB_LOCAL < ./PERMISSIONS/create_role_psswd.sql > create_role_psswd.out;
 sqlplus psswd_user/$HDB_LOCAL < ./PERMISSIONS/check_valid_role_name.prc > check_valid_role_name.out;
 sqlplus psswd_user/$HDB_LOCAL < ./PERMISSIONS/role_psswd_fk.trg > role_psswd_fk.out;
-sqlplus sys/hdb2 @./PERMISSIONS/create_app_user.sql $HDB_LOCAL > create_app_user.out;
-sqlplus sys/hdb2 < ./PERMISSIONS/local_user.sql > local_user.out
+sqlplus 'uchdba/uchdbdev as sysdba' @./PERMISSIONS/create_app_user.sql $HDB_LOCAL > create_app_user.out;
+sqlplus 'uchdba/uchdbdev as sysdba' < ./PERMISSIONS/local_user.sql > local_user.out
