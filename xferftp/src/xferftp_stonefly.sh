f69d5b10eaece782efc4802660e95f96d65bcf6f
agilmore <>
2005-06-03 19:20:13 +0000

    Transfer paradox directory as well.

--- xferftp/src/xferftp_stonefly.sh
+++ xferftp/src/xferftp_stonefly.sh
@@ -2,3 +2,4 @@
 ncftpput -e sevier.ftperr -m -f account.dat sevier/ /zipdata/slc/xfer/sevier/*.xfr
 ncftpput -e weber.ftperr -m -f account.dat weber/ /zipdata/slc/xfer/weber/*.xfr
 ncftpput -e price.ftperr -m -f account.dat price/ /zipdata/slc/xfer/price/*.xfr
+ncftpput -e paradox.ftperr -m -f account.dat paradox/ /zipdata/slc/xfer/paradox/*.xfr
