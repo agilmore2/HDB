fde79e922ad44a01d4b4d733361f25f0d3498e0f
agilmore <>
2005-10-26 22:22:13 +0000

    Altered to use account.dat file, and ignore extra files

--- elpaso/src/get_elpaso.sh
+++ elpaso/src/get_elpaso.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 #
-# first line lets shell know that this is a C script
-# the -f tells the shell not to source the .cshrc file
-# thus none of the aliases in that file will be valid
-# the -x causes an echo of commands to standard output
-#
 # go get data from El Paso
 #
 echo ' ' 
@@ -12,12 +7,9 @@ echo '     Attempting to log on to ElPaso computer'
 echo ' '
 umask 022
 
-FTP_HOST=140.217.80.16 # elpsun1.rgpo.uc.usbr.gov
 FTP_DIR=/usr1/home/jgrajeda/website
-FTP_USER=waterguy
-FTP_PASSWD=CeeSicky2
 
-ncftpget -u $FTP_USER -p $FTP_PASSWD $FTP_HOST . $FTP_DIR/Dailyflo.htm
+ncftpget -e elpaso.ftperr -f account.dat . $FTP_DIR/Dailyflo.htm
 
 echo ' '
 echo '     FTP Processing complete'
