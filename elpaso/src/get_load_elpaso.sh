e70809c79566d7cad63a5162edf3fe939c906eaf
agilmore <>
2005-10-28 17:28:11 +0000

    We have been processing the same datafile since April 6. Ouch. Fixed

--- elpaso/src/get_load_elpaso.sh
+++ elpaso/src/get_load_elpaso.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 #delete the files
-\rm -f Dailyflo.htm
+\rm -f Dailyflow.htm
 touch marker.delete
 #activate the get_snotel script which get the data from portland
 $HDB_ENV/elpaso/src/get_elpaso.sh
 #see if new files have come back
 newest=`ls -t |head -1`
-if [ $newest != 'marker.delete' ]
+if [ $newest = 'Dailyflow.htm' ]
 then
   echo ' '
   echo '     new files were retrieved from ElPaso  -> continue processing'
   echo ' ' 
   . $HDB_ENV/.bashrc_hdb_app
-  $HDB_ENV/bin/elpaso2hdb.pl Dailyflo.htm app_user uchdb2
+  $HDB_ENV/bin/elpaso2hdb.pl Dailyflow.htm app_user uchdb2
 else 
   echo 'FTP transfer failed!'
   exit 1
