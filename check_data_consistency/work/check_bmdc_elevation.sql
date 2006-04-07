48b5ab5697cea4b7757575b30d2ba9b554188af2
agilmore <>
2006-04-07 16:30:43 +0000

    Added check for 06 DST

--- check_data_consistency/work/check_bmdc_elevation.sql
+++ check_data_consistency/work/check_bmdc_elevation.sql
@@ -21,8 +21,8 @@ a.interval = 'instant' and
 b.interval = 'hour' and 
 a.collection_system_id in (2,3) and -- Dayfile
 b.collection_system_id = 7 and -- SCADA
-((a.start_date_time = (b.start_date_time + 1/24) and a.start_date_time >= '30-OCT-2005') or
-(a.start_date_time = (b.start_date_time + 2/24) and a.start_date_time < '30-OCT-2005')) and 
+((a.start_date_time = (b.start_date_time + 1/24) and a.start_date_time between '30-OCT-2005' and '01-APR-2006') or
+(a.start_date_time = (b.start_date_time + 2/24) and (a.start_date_time < '30-OCT-2005' or a.start_date_time >= '02-apr-2006'))) and 
 /* SCADA end date time is one hour off of instantaneous data
    DST issue, so when DST changes, this needs to get altered */
 abs(a.value - b.value) > .031 and -- difference of 0.031 feet is alarm
@@ -53,8 +53,8 @@ a.interval = 'instant' and
 b.interval = 'hour' and
 a.collection_system_id in (2,3) and
 b.collection_system_id = 7 and
-((a.start_date_time = (b.start_date_time + 1/24) and a.start_date_time >= '30-OCT-2005') or
-(a.start_date_time = (b.start_date_time + 2/24) and a.start_date_time < '30-OCT-2005')) and
+((a.start_date_time = (b.start_date_time + 1/24) and a.start_date_time between '30-OCT-2005' and '01-APR-2006') or
+(a.start_date_time = (b.start_date_time + 2/24) and (a.start_date_time < '30-OCT-2005' or a.start_date_time >= '02-apr-2006'))) and 
 abs(a.value - b.value) > .031 and
 a.start_date_time > sysdate - &&days_back and
 c.site_datatype_id = a.site_datatype_id and
