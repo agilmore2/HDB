26741188c019f1f6398d427ec99693b6abcf27aa
agilmore <>
2006-05-16 16:13:47 +0000

    Updated limit to ignore .04 feet. May also be DST changes.

--- check_data_consistency/work/check_bmdc_elevation.sql
+++ check_data_consistency/work/check_bmdc_elevation.sql
@@ -25,7 +25,7 @@ b.collection_system_id = 7 and -- SCADA
 (a.start_date_time = (b.start_date_time + 2/24) and (a.start_date_time < '30-OCT-2005' or a.start_date_time >= '02-apr-2006'))) and 
 /* SCADA end date time is one hour off of instantaneous data
    DST issue, so when DST changes, this needs to get altered */
-abs(a.value - b.value) > .031 and -- difference of 0.031 feet is alarm
+abs(a.value - b.value) > .041 and -- difference of 0.031 feet is alarm
 a.start_date_time > sysdate - &&days_back and 
 c.site_datatype_id = a.site_datatype_id and 
 c.datatype_id in (24) and -- instant elevation
@@ -55,7 +55,7 @@ a.collection_system_id in (2,3) and
 b.collection_system_id = 7 and
 ((a.start_date_time = (b.start_date_time + 1/24) and a.start_date_time between '30-OCT-2005' and '01-APR-2006') or
 (a.start_date_time = (b.start_date_time + 2/24) and (a.start_date_time < '30-OCT-2005' or a.start_date_time >= '02-apr-2006'))) and 
-abs(a.value - b.value) > .031 and
+abs(a.value - b.value) > .041 and
 a.start_date_time > sysdate - &&days_back and
 c.site_datatype_id = a.site_datatype_id and
 c.datatype_id in (24) and
