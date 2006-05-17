290390425f3f20e1ceed656ec76f898226cc46e0
agilmore <>
2006-05-17 21:03:56 +0000

    changed directory location for properties files

--- dataloader/work/realtime/hmKillRealtimeJava.sh
+++ dataloader/work/realtime/hmKillRealtimeJava.sh
@@ -2,7 +2,7 @@
 #setup java commandline
 #HDB_XFER_EMAIL is the email of the person (NOT uchdba) that is responsible
 # for the HDB data loading application
-JAVA_ARGS="-Demailaddr=$HDB_XFER_EMAIL -Dstart.property=$HDB_ENV/dataloader/realtimeloader.property"
+JAVA_ARGS="-Demailaddr=$HDB_XFER_EMAIL -Dstart.property=$HDB_ENV/dataloader/work/realtime/realtimeloader.property"
 
 java $JAVA_ARGS dataloader.KillServer $1
 exit 0;
