748a47273ada54fe8cad0f6ad3a8f02bdfd22920
agilmore <>
2007-02-01 20:28:39 +0000

    Altered to use days in month of yesterday instead of today minus one. Left blanks in.

--- xferftp/src/xfermail_montrose.sh
+++ xferftp/src/xfermail_montrose.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-days=`date +%d`
-
-days=`expr $days - 1`
+days=`/usr/local/bin/date +%d -d 'yesterday'`
 
 #echo $days
 
@@ -11,9 +9,9 @@ site_data 919 42 $days |\grep -v ORACLE > temp.out
 echo "Powell Release	acre-ft" >powellrel
 
 #due to the old geezer, we are using 1.9835 here to match Hydromet
-grep -v -- --- temp.out | awk '/-/ {s += $2 ; printf "%s\t%d\n",$1,$2*1.9835} END {printf "Monthly Total is:\t%s\n",s*1.9835}' >>powellrel
+cat temp.out | awk '/-200/ {s += $2 ; printf "%s\t%d\n",$1,$2*1.9835} END {printf "Monthly Total is:\t%s\n",s*1.9835}' >>powellrel
 
 mailx -s "Powell Release" crane@wapa.gov,telschow@wapa.gov,tryan@uc.usbr.gov <powellrel
 
-rm powellrel temp.out
+#rm powellrel temp.out
 
