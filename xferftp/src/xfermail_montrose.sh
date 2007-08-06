d131721e87a357ccb78bb28505541f416a553dd2
agilmore <>
2007-08-06 16:41:49 +0000

    added Jennifer Turnbull to WAPA recipients.

--- xferftp/src/xfermail_montrose.sh
+++ xferftp/src/xfermail_montrose.sh
@@ -11,7 +11,7 @@ echo "Powell Release	acre-ft" >powellrel
 #due to the old geezer, we are using 1.9835 here to match Hydromet
 cat temp.out | awk '/-200/ {s += $2 ; printf "%s\t%d\n",$1,$2*1.9835} END {printf "Monthly Total is:\t%s\n",s*1.9835}' >>powellrel
 
-mailx -s "Powell Release" crane@wapa.gov,telschow@wapa.gov,tryan@uc.usbr.gov <powellrel
+mailx -s "Powell Release" crane@wapa.gov,telschow@wapa.gov,tryan@uc.usbr.gov,turnbull@wapa.gov <powellrel
 
 #rm powellrel temp.out
 
