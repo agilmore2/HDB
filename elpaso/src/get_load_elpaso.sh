cfc3a923e6e287513b821b5b56c2cacd887defa4
agilmore <>
2008-03-28 17:06:50 +0000

    deal with log file messing up download detection

--- elpaso/src/get_load_elpaso.sh
+++ elpaso/src/get_load_elpaso.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 #delete the files
 \rm -f Dailyflow.htm
-touch marker.delete
 #activate the get_snotel script which get the data from portland
 $HDB_ENV/elpaso/src/get_elpaso.sh
-#see if new files have come back
-newest=`ls -t |head -1`
-if [ $newest = 'Dailyflow.htm' ]
+#see if a file came back
+if [ -f 'Dailyflow.htm' ]
 then
   echo ' '
   echo '     new files were retrieved from ElPaso  -> continue processing'
@@ -17,7 +15,6 @@ else
   echo 'FTP transfer failed!'
   exit 1
 fi
-\rm marker.delete
 echo ' ' 
 echo '     The script - get_load_elpaso - is  complete'
 echo ' '
