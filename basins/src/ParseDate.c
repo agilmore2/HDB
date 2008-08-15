6008a8d1f3b9d779d77cf1001353c6b9f4c5e570
agilmore <>
2008-08-15 21:52:29 +0000

    Do a better job of string processing

--- basins/src/ParseDate.c
+++ basins/src/ParseDate.c
@@ -34,6 +34,8 @@ int parseDate (char *dateString, char *oracleDate)
 
 {
   char *tempDate,
+       tempDate1[20],
+       tempDate2[20],
        day[11],
        month[25],
        year[8];
@@ -42,31 +44,31 @@ int parseDate (char *dateString, char *oracleDate)
 
   /* Get the month */
   tempDate = strstr(dateString, ":");
-  strcpy (tempDate, &(tempDate[2]));
-  strcpy (month, tempDate);
+  strcpy (tempDate2, &(tempDate[2]));
+  strcpy (month, tempDate2);
 
   i = 0;
   while (month[i] != ' ')
     i++;
   month[i] = '\0';
-  strcpy (tempDate, &(tempDate[i + 1])); 
+  strcpy (tempDate1, &(tempDate2[i + 1])); 
 
 
   /* Get the day */
-  strcpy (day, tempDate);
+  strcpy (day, tempDate1);
 
   i = 0;
   while (day[i] != ' ')
     i++;
   day[i] = '\0';
-  strcpy (tempDate, &(tempDate[i + 3])); 
+  strcpy (tempDate2, &(tempDate1[i + 3])); 
 
 
   /* Get the year */
-  strcpy (year, tempDate);
+  strcpy (year, tempDate2);
 
   i = 0;
-  while (year[i] != ' ')
+  while (year[i] != '\n' && year[i] != ' ')
     i++;
   year[i] = '\0';
 
