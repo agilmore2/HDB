dbd0626487e30e55d57cae94a7d154b2010991d6
Andrew Gilmore <andrew@precisionwre.com>
2019-11-15 14:54:26 -0700

    Updated debug invocation comment

--- dataloads/CUWCD/src/process_CUWCD_data_file.sh
+++ dataloads/CUWCD/src/process_CUWCD_data_file.sh
@@ -41,7 +41,7 @@ wget -O $CUWCD_DATA_FILE $CUWCD_DATA_URL
 # if the java routine successfully  processed this file then zip the file to indicate it was processed
 
   echo " Processing CUWCD file: $CUWCD_DATA_FILE "
-#  java -Doracle.jdbc.Trace=true -Djava.util.loggin.config.file=log.properties -Dstart.property=$CUWCD_DATA_PROP dataloader.ProcessUSACEfile $CUWCD_DATA_FILE
+#   java -Doracle.jdbc.Trace=true -Djava.util.logging.config.file=../src/log.properties -Dstart.property=../src/CUWCD_data.property dataloader.ProcessUSACEfile CUWCD_data.2019110320
   java -Dstart.property=$CUWCD_DATA_PROP dataloader.ProcessUSACEfile $CUWCD_DATA_FILE
 
   if [ $? -eq 0 ]
