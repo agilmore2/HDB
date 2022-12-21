204a51a0c837c8f8ddd0838a6f5bf1b93636f6df
xcellious <38482887+xcellious@users.noreply.github.com>
2022-12-20 22:43:03 -0500

    leftover dest/source objects removal

--- oracle_script/Version_DDLs/4.15/comments_tables_columns.sql
+++ oracle_script/Version_DDLs/4.15/comments_tables_columns.sql
@@ -1,7 +1,7 @@
 --------------------------------------------------------------------------
 -- This script add comments on HDB_ REF_ tables and columns
 -- IMPORTANT!!! This script should be executed as main schema user that owns the tables.
--- created on 12/17/2022 by IsmailO
+-- created on 12/21/2022 by IsmailO
 --------------------------------------------------------------------------
 
 -- comments on tables and columns
@@ -10,7 +10,7 @@
 
 
 --update version table
-INSERT INTO TSDB_PROPERTY (PROP_NAME, PROP_VALUE) VALUES ('4.15.3', 'comments on HDB_ REF_ tables and columns have been added. ');
+INSERT INTO TSDB_PROPERTY (PROP_NAME, PROP_VALUE) VALUES ('4.15.4', 'comments on HDB_ REF_ tables and columns have been added. ');
 /
 
 --Commit
