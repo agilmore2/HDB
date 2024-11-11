8c828e0b0fe93c710634f99fae64fd62f00fc0ce
Andrew Gilmore <andrew@precisionwre.com>
2024-11-11 16:24:55 -0700

    Moved obsolete code to separate repo

--- 24moFld/src/FormatOutput.h
+++
@@ -1,5 +0,0 @@
-#define NUM_MONTH 12
-
-char * pred = "* * * * P R E D I C T E D   S P A C E * * * *";
-char * cred = "* * * * C R E D I T A B L E   S P A C E * * * *";
-char * effe = "* * * * E F F E C T I V E   S P A C E * * * *";
