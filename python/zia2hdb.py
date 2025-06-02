3a904037adae8ec9f76b6ad933811814d615a685
AndrewPWRE <andrew@precisionwre.com>
2025-06-01 23:57:11 -0600

    Mostly Working hardcoded loading script. Actual writing seems to fail.

--- python/zia2hdb.py
+++ python/zia2hdb.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
-'''HDB writing from XLSX files, designed specifically for CUL project
-Excel input files should be in the following format:
-    - sheet/row/column => datatype/timestep/site
-    - header rows => either one set of headers that apply to all sites in the sheet or one set of headers per site
-    - data flags => for time series data flags, each site should have a column to the right with data flags and a column header <site>.data_flags
+"""HDB writing from NMSU Ziamet for Nambe Falls climate data
 
+Built against Python 3.12 and cx_Oracle 8.3.0
+Created May/June 2025
 
-Built against Python 3.9 and cx_Oracle 5.1.2
-Created February 2022
-
-Has only been used for the CUL project, could use better error handling
-@author: Andrew Gilmore, Brett Boyer
-'''
+@author: Andrew Gilmore
+after xl2hdb by Andrew Gilmore and Brett Boyer
+"""
 import io
-from datetime import datetime
+from datetime import datetime, timedelta
 import sys
 import argparse
 import os
@@ -21,9 +16,9 @@ import pandas as pd
 import requests
 
 from lib.hdb import Hdb
-import csv
 
-def debug(msg,v):
+
+def debug(msg, v):
     if v: print(msg)
 
 
@@ -34,65 +29,144 @@ def generate_url():
 def get_file(url):
     return url
 
+def parse_dates(args):
+    # handle dates:
+    # Truth table and results for numdays, begindate and enddate
+    # errors are all defined, only enddate defined, nothing defined
+    # everything else gets fixed up.
+    # Num Days | Beg Date | End Date | Result
+    # nothing defined                   error
+    #  def                              end date = now, beg date = enddate-numdays
+    #  def        def                   end date = beg date + numdays
+    #             def                   end date = now, check max num days?
+    #             def         def       ok
+    #                         def       error, what is beg date?
+    #  def                    def       beg date = end date - numdays
+    #  def        def         def       error, not checking to see if consistent
+    args.numdays = int(args.numdays)
+
+    if args.numdays:
+        # let exceptions handle if arguments are nonsense
+        if args.begindate and args.enddate:
+            print ("Error, overspecified dates, all of -n, -b, -e specified!")
+            exit(1)
+        elif args.begindate:
+            args.begindate = datetime.fromisoformat(args.begindate)
+            args.enddate = args.begindate + datetime.timedelta(args.numdays)
+        elif args.enddate:
+            args.enddate = datetime.fromisoformat(args.enddate)
+        else:
+            args.enddate = datetime.now().date()
+        # also handles if enddate is specified
+        args.begindate = args.enddate + timedelta(days=-args.numdays)
+
+    else:
+        if args.begindate and args.enddate:
+            args.begindate = datetime.fromisoformat(args.begindate)
+            args.enddate = datetime.fromisoformat(args.enddate)
+        elif args.begindate:
+            args.begindate = datetime.fromisoformat(args.begindate)
+            args.enddate = datetime.now()
+        elif args.enddate:
+            print("Error, cannot specify only end date!")
+            exit(1)
+        else:
+            print("Error, dates are unspecified!")
+            exit(1)
+        args.numdays = args.enddate - args.begindate
+
+    if not args.begindate or not args.enddate:
+        print("Error, dates not specifed or error parsing dates!")
+        exit(1)
+
+    return args
+
+
 def main(args):
     parser = argparse.ArgumentParser()
-    #parser.add_argument('-a', '--authFile', help='app_login containing database credentials', required=True)
+    parser.add_argument('-a', '--authFile', help='app_login containing database credentials', required=True)
     parser.add_argument('-f', '--file', help='File to load from', required=False)
-    parser.add_argument('-w', '--web', help='Load from web as defined in HDB', required=False)
+    parser.add_argument('-n', '--numdays', help='Number of days to load', required=False)
+    parser.add_argument('-b', '--begindate', help='Beginning date to load from, YYYY-MM-DD', required=False)
+    parser.add_argument('-e', '--enddate', help='Ending date to load to, YYYY-MM-DD', required=False)
     parser.add_argument('--verbose', action='store_true', help='Show more detail about process')
     parser.add_argument('--overwrite', action='store_true', help='Write an O to the overwrite_flag field')
-    args = parser.parse_args()
 
-    debug(args,args.verbose)
+    args = parse_dates(parser.parse_args())
+
+    debug(args, args.verbose)
 
     if not args.file:
         #url = generate_url(ids)
         url = 'https://weather.nmsu.edu/ziamet/station/sfwx/dly/sfnamb/'
         client=requests.session()
-        client.get(url)
-        response = client.post(url, headers = {
-            'Referer': 'https://weather.nmsu.edu/ziamet/station/data/sfnamb/',
-            },
-                               data = {
-                                   'csrfmiddlewaretoken': client.cookies['csrftoken'],
-                                   'dtype': 'sfwx',
-                                   'sid': 'sfnamb',
-                                   'sdate': '2025-04-30',
-                                   'edate': '2025-05-31',
-                                   'output': 'csv',
-                                   'units': 'iu',
-                               }
+        client.get(url) #get CSRF cookie
+        response = client.post(url,
+                               headers={'Referer': 'https://weather.nmsu.edu/ziamet/station/data/sfnamb/', },
+                               data={'csrfmiddlewaretoken': client.cookies['csrftoken'],
+                                                   'dtype': 'sfwx',
+                                                     'sid': 'sfnamb',
+                                                   'sdate': args.begindate,
+                                                   'edate': args.enddate,
+                                                  'output': 'csv',
+                                                   'units': 'iu',
+                                    }
                                )
 
         csvfile = io.StringIO(response.content.decode('utf-8'))
     else:
         csvfile = args.file
 
-#    print(csvfile.read())
     df = pd.read_csv(csvfile, skiprows=0)
-    #print(df)
 
-
-    #delete unit row
+    # delete unit row
     df = df.drop(0).reset_index(drop=True)
 
-    #make date time without deprecation warnings
-    #df['Date_Time'] = pd.to_datetime(df['Date'] + ' ' + df['Time'], format='%Y-%m-%d %H:%M')
-
     #delete extra columns and any NaN rows
-    df=df.loc[:,df.columns.str.contains('(?:Date)|(?:Air Temperature)|(?:Rain)')].dropna()
-    print(df)
+    df = df.loc[:, df.columns.str.contains('(?:Date)|(?:M.. Air Temperature)|(?:Rain)')].dropna()
+    df['Date'] = pd.to_datetime(df['Date'])
+    df[["Rain", "Max Air Temperature", "Min Air Temperature"]] =\
+        df[["Rain", "Max Air Temperature", "Min Air Temperature"]].apply(pd.to_numeric)
 
-    exit()
+    debug(df, args.verbose)
 
-    # Write either None or 'O' to the overwrite flag field
-    oFlag = 'O' if args.overwrite else None
+    dtList = df["Date"].tolist()
+    rainList = df["Rain"].tolist()
+    maxTList = df["Max Air Temperature"].tolist()
+    minTList = df["Min Air Temperature"].tolist()
+
+    #hardcoded for now
+    rainSDI = 19698
+    maxSDI = 20200
+    minSDI = 20201
 
     db = Hdb()
     db.connect_from_file(args.authFile)
 
+    interval = 'day'
+    db.agen = 'New Mexico State University'
+    db.app = os.path.basename(sys.argv[0])
+    db.collect = '(see agency)'
+    db.method = 'N/A'
+    db.comp = 'unknown'
+    # Write either None or 'O' to the overwrite flag field
+    oFlag = 'O' if args.overwrite else None
+
     ids = db.get_app_ids()
 
+    db.write_xfer(ids | {'sdi': rainSDI, 'inter': interval,
+                                      'overwrite_flag': oFlag, 'val': 'Z'},
+                  dtList, rainList)
+
+    db.write_xfer(ids | {'sdi': maxSDI, 'inter': interval,
+                                      'overwrite_flag': oFlag, 'val': 'Z'},
+                  dtList, maxTList)
+
+    db.write_xfer(ids | {'sdi': minSDI, 'inter': interval,
+                                      'overwrite_flag': oFlag, 'val': 'Z'},
+                  dtList, minTList)
+
+    print("Wrote %d values." % (len(rainList) + len(maxTList) + len(minTList)))
 
 
 if __name__ == '__main__':
