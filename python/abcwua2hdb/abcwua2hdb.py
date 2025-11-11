997ebbe82b678f0064f561a91f67f79d36214adc
agilmore2 <agilmore2@gmail.com>
2025-11-11 16:25:51 -0700

    Working hourly/daily loader

--- python/abcwua2hdb/abcwua2hdb.py
+++ python/abcwua2hdb/abcwua2hdb.py
@@ -6,8 +6,8 @@ import argparse
 import os
 from lib.hdb import Hdb
 import pandas
-
 import requests
+
 #import logging
 #from http.client import HTTPConnection
 
@@ -17,8 +17,9 @@ import requests
 
 
 ''' HDB writing from ABCWUA data
-    Only one site, two SDIS, diversion and return flow volumes
-    "We can ignore column B and D (units in KG). I just want to pull in columns C and E. C would go to SDI 31132, E would go to SDI 20071
+    Only one or two sites, two SDIS, diversion and return flow volumes per hourly and daily
+    daily - "We can ignore column B and D (units in KG). I just want to pull in columns C and E. C would go to SDI 31132, E would go to SDI 20071"
+    hourly - "We only need columns E (Total Diverted (AF)) and I (Total Returned (AF)) It should be 31133 and 31134."
 '''
 
 
@@ -39,12 +40,13 @@ def main(args):
                 
     parser.register('action', 'validate_date', ValidateDate)
 
-    parser.add_argument('-a', '--authFile', help='app_login containing database credentials', required=True)
+    parser.add_argument('-a', '--authFile', help='File containing database credentials', required=True)
+    parser.add_argument('-l', '--flowType', help='Daily(d) or hourly(h) data?', required=True)
     parser.add_argument('--overwrite', action='store_true', help='Write an O to the overwrite_flag field')
-    parser.add_argument('-n', '--numdays', help='How many days to load', required=False)
-    parser.add_argument('-b', '--begin', action='validate_date', help='Begin date (YYYY-MM-DD)', required=False)
-    parser.add_argument('-e', '--end', action='validate_date', help='End date (YYYY-MM-DD)', required=False)
-    parser.add_argument('-d', '--verbose', action='store_true', help='Show more detail about process')
+    parser.add_argument('-n', '--numdays', help='How many days to load, some combination of -b, -e, -n is required to define date range')
+    parser.add_argument('-b', '--begin', action='validate_date', help='Begin date (YYYY-MM-DD)')
+    parser.add_argument('-e', '--end', action='validate_date', help='End date (YYYY-MM-DD)')
+    parser.add_argument('-d', '--verbose', action='store_true', help='Show debug info')
     parser.add_argument('-t', '--test', action='store_true', help='Test, do not write to database')
 
     args = parser.parse_args()
@@ -59,13 +61,14 @@ def main(args):
         return list(abcwua_sites.keys())
     
     # Python equivalent of Perl get_abcwua_sites
-    def get_abcwua_sites(hdb):
+    def get_abcwua_sites(hdb, flowtype):
         """
         Query HDB for abcwua sites and return a nested dict keyed by site_num and data_code.
         There is only one site from this source.
         """
         # Build id_limit_clause
-        title = 'ABCWUA Daily Loader'
+        # choose title based on flow type (daily vs instantaneous)
+        title = 'ABCWUA Daily Loader' if flowtype == 'd' else 'ABCWUA Hourly Loader'
         params = {'title': title}
         sql = f"""
             select b.primary_site_code as abcwua_id, b.primary_data_code as data_code,
@@ -100,7 +103,9 @@ def main(args):
     # All dates are datetime.date
     def determine_date_range(args):
         lastDay = datetime.today().date()
-        lastDay = lastDay - pandas.Timedelta(days=1)
+        # For daily data, use yesterday as the last available day
+        if args.flowType == 'd':
+            lastDay = lastDay - pandas.Timedelta(days=1)
 
         begin = args.begin
         end = args.end
@@ -146,13 +151,10 @@ def main(args):
     db.app_id= db.get_loadingAppID(db.app)
     debug(f"Using loading application {db.app} with id {db.app_id}", args.verbose)
 
-    sites=get_abcwua_sites(db)
+    sites=get_abcwua_sites(db, args.flowType)
     debug(sites, args.verbose)
     debug(f"abcwua Sites to retrieve: {build_site_num_list(sites)}", args.verbose)
 
-    #debug(abcwua.fetch_locations())
-    #debug((sites=build_site_num_list(sites),parameterCd=build_site_code_list(sites),
-    #                      start=begin,end=end,service='dv'), args.verbose)
     query = {
             "startTime": begin,
             "endTime": end,
@@ -160,42 +162,54 @@ def main(args):
     site = list(sites.keys())[0]
     firstcode = list(sites[site].keys())[0]
     url = sites[site][firstcode]["url"]
-    debug(f"URL to fetch data from is {url}", args.verbose)
+    # parse datetimes like "2025-09-15" for daily data or "2025-09-15 12:00 am" for hourly
+    date_format = '%Y-%m-%d' if args.flowType == 'd' else '%Y-%m-%d %I:%M %p'
     try:
         response = requests.get(url, params=query)
-        df = pandas.read_excel(response.content, index_col=0, parse_dates=True, usecols=[0,2,4])
+        df = pandas.read_excel(response.content, index_col=0, parse_dates=True, date_format=date_format)
     except Exception as e:
         print(f"Error fetching data for {url}: {e}: ", file=sys.stderr)
         db.rollback()
         sys.exit(1)
 
-    debug(f"Writing data for site {site}", args.verbose)
+    # Find columns that match mappings
+    # Match is case-insensitive and trims whitespace.
+    # Collect all unique data_code keys across all sites (preserving order)
+    codes = list(dict.fromkeys(k for s in sites.values() for k in s.keys()))
+    df_cols = [str(c) for c in df.columns]
+    matched = []
+    for key in codes:
+        key_s = str(key).strip().lower()
+        for col in df_cols:
+            if col.strip().lower() == key_s:
+                matched.append(col)
+                break
+
+    if not matched:
+        debug(f"No matching columns found in dataframe for site keys: {codes}", args.verbose)
+    else:
+        debug(f"Matched columns: {matched}", args.verbose)
+
+    # cut out matching columns, timestamp index automatically kept
+    df = df.loc[:, matched]
+    debug(f"Sliced dataframe columns: {list(df.columns)}", args.verbose)
 
     debug(df, args.verbose)
     print(f'Size of data retrieved for {site} is {len(df)}')
     dt_list = df.dropna().iloc[:].index.tolist()
-    div_list = df.dropna().iloc[:, 0].tolist()
-    ret_list = df.dropna().iloc[:, 1].tolist()
     debug(dt_list, args.verbose)
-    debug(div_list, args.verbose)
-    debug(ret_list, args.verbose)
 
-# try to handle cleanly when sites is either one or two sites
+    # try to handle cleanly when sites may be one or many sites
     for site in sites:
-        code = list(sites[site].keys())[0]
-        if (code == "Total Diverted (AF)"): 
-           db.write_xfer(sites[site][code] | {
-                'overwrite_flag': oFlag, 'val': None, 'app_id': db.app_id},
-                dt_list, div_list)
-        code = list(sites[site].keys())[-1]
-        if (code == "Total Return (AF)"):
+        for code in sites[site]:
+            data_list = df.dropna().loc[:, code].tolist()
             db.write_xfer(sites[site][code] | {
-                'overwrite_flag': oFlag, 'val': None, 'app_id': db.app_id},
-                dt_list, ret_list)
-    
+                    'overwrite_flag': oFlag, 'val': None, 'app_id': db.app_id},
+                    dt_list, data_list)
+        
     if args.test:
         db.rollback()
-        debug("Test mode active, database rollback executed.", args.verbose)
+        debug("Test mode active, database rolled back.", args.verbose)
     else:
         db.commit()
         debug("Data written to database.", args.verbose)
